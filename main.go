9bff24059ddeff61f849941366a3267b319ecac0
Alexander <sunny.os87@rambler.ru>
2020-08-07 21:51:33 +1000

    connect to mongodb

--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 	"os"
 	"time"
+	
+	//"go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	clientsOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientsOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
 	r := mux.NewRouter()
 
-	r.Handle("/get-token", http.HandlerFunc(GetTokenHandler)).Methods("GET")
 	r.Handle("/", http.FileServer(http.Dir("./views/"))).Methods("GET")
 	r.Handle("/users/{name}", http.HandlerFunc(UserHandler)).Methods("GET")
+
+	r.Handle("/get-token", http.HandlerFunc(GetTokenHandler)).Methods("GET")
+	r.Handle("/users/{guid}/tokens", http.HandlerFunc(GenerateTokenHandler)).Methods("POST")
 
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
 		http.FileServer(http.Dir("./static/")))).Methods("GET")
@@ -32,6 +51,19 @@ func UserHandler(w http.ResponseWriter, r *http.Request) {
 var mySigningKey = []byte("secret")
 
 func GetTokenHandler(w http.ResponseWriter, r *http.Request) {
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"admin": true,
+			"name": "Alex O",
+			"exp": time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, _ := token.SignedString(mySigningKey)
+	fmt.Fprintf(w, tokenString)
+}
+
+func GenerateTokenHandler(w http.ResponseWriter, r *http.Request) {
 	token := jwt.NewWithClaims(
 		jwt.SigningMethodHS256,
 		jwt.MapClaims{
